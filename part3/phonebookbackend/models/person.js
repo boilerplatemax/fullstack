02a5a163a3fc4b37d66adb44ca077de98c3883cc
MaxShapovalov <55261865+MaxShapovalov@users.noreply.github.com>
2022-10-25 16:43:29 -0500

    part 4

--- part3/phonebookbackend/models/person.js
+++ part3/phonebookbackend/models/person.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
+const url = process.env.MONGODB_URI;
+console.log('connecting to', url);
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('connected to MongoDB')
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -25,15 +25,15 @@ const personSchema = new mongoose.Schema({
     minlength: 7,
     required: true
   }
-})
+});
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 personSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
