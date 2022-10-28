f56f9190d3f3a933c948aa06b66e40ac8cd31eba
MaxShapovalov <55261865+MaxShapovalov@users.noreply.github.com>
2022-10-27 22:11:40 -0500

    4.18-4.19

--- part4/part4blogs/controllers/blogs.js
+++ part4/part4blogs/controllers/blogs.js
@@ -2,6 +2,15 @@ const blogRouter = require('express').Router()
 const logger = require('../utils/logger')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+  }
 
 blogRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id:1})
@@ -10,7 +19,11 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response, next) => {
     const body = request.body
-
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
     const users = await User.find({})
     const currentUser = users.filter(u=>u.username===body.username)[0]
     
